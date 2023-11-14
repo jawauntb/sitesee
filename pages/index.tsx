7f76afaa2be2dd3e17fa9fac87c1f3cfc8b91f93
jawauntb <jawaun.brown95@gmail.com>
2023-11-14 21:23:00 +0000

    added img upload and reprod. ability

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Head from 'next/head';
 
 export default function Home() {
@@ -10,14 +10,52 @@ export default function Home() {
   const [generatedImagePrompt, setGeneratedImagePrompt] = useState('')
   const [isLoading, setIsLoading] = useState(false);
   const [imagesLoading, setImagesLoading] = useState(false);
+  const [imageDescription, setImageDescription] = useState('');
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const fileInputRef = useRef(null);
 
-  const designGuidelines = "; return html with proper js and tailwinds functionality as a response. Make sure you use modern design with rounded edges, drop shadows, unified color palette of 3 colors, buttons scale on hover. Use FontAwesome for icons (<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"> and any font use GoogleFont.everything needs padding of 8px and margin of 5px. should be mobile friendly too; make sure to add descriptive site content based on input, insert the img urls we pass to you; dont leave content empty--make it realistic; Now write our landing.html, make up stuff if needed for realistic copy; mobile friendly also;";
+
+  const designGuidelines = "Create a responsive HTML site with JavaScript and Tailwind CSS. Design must be modern: include rounded edges, drop shadows, and a harmonized color scheme with three colors. Enhance interactivity with hover effects on buttons.Use FontAwesome for icons (<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"> and Google Fonts for typography. Apply consistent padding of 8px and margin of 5px across all elements. Ensure mobile compatibility. The site content should be descriptive and realistic, incorporating provided image URLs where relevant. Fabricate content for a realistic feature or site experience. make sure to use interesting shapes or icons, dont be afraid to draw shapes, but all must be in one html tag space";
   const useImgsPrompt = (imgUrls: string) => `7.find some way to use these images:${imgUrls}`;
   const insertOneImg = ', Find a good way to use this image:'
   const insertTwoImg = ', Find a good way to use these images:'
+
   function extractHTML(response: string) {
     const match = response.match(/```html([\s\S]*?)```/);
     return match ? match[1].trim() : '';
+  }
+
+  async function handleImageUpload(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('image', file);
+      setUploadedImage(file);
+      try {
+        const response = await fetch('https://emojipt-jawaunbrown.replit.app/analyze_uploaded_image', {
+          method: 'POST',
+          body: formData,
+        });
+
+        const data = await response.json();
+        if (data) {
+          const imageDescription = data.choices[0].message.content;
+          console.log('imageDescription', imageDescription);
+          setImageDescription(imageDescription); // Set state for image description
+        }
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  }
+
+  function removeUploadedImage() {
+    setUploadedImage(null);
+    setImageDescription(''); // Reset image description if you are storing it
+    // Reset the file input
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
   }
 
   async function makeAPIRequest(payload) {
@@ -84,30 +122,27 @@ export default function Home() {
   async function handleSubmit() {
     setIsLoading(true);
 
-    // Get the image prompt based on website idea
-    // const newPrompt = await getImagePrompt();
+    // Base prompt with design guidelines
+    let promptContent = `create in html + tailwinds; it should be a rich feature or complete site; follow these specifications-> ${designGuidelines};`;
 
-    // // Fetch images
-    // const fetchedImageUrls = await fetchImages(newPrompt);
-    // setImageUrls(fetchedImageUrls);
+    // Append site idea if present
+    if (!!websiteIdea) {
+      const shortenedSiteIdea = getShortenedText(websiteIdea, 500);
+      promptContent += ` site idea: ${shortenedSiteIdea};`;
+    }
 
-    const shortenedSiteIdea = getShortenedText(websiteIdea, 500);
-    const imgUrls = imageUrls.join(' ');
-    // Create a single combined prompt for the entire site
-    const combinedPromptContent = ` make a site in html + tailwinds for this site:
-      site idea: ${shortenedSiteIdea};
-      follow these specificiations->
-      1.create a header + navigation section.;
-      2.a hero section with a value prop;
-      3.a section with buttons or quotes;
-      4. a content section.;
-      5.a footer section.;  8..here are some rules for your response: ${designGuidelines};
-    `;
+    // Append image description if present
+    if (imageDescription) {
+      promptContent += ` Uploaded UI image description: ${imageDescription};`;
+    }
+
+    // Construct the final combined prompt
+    const combinedPromptContent = promptContent;
 
     const rawContent = await makeAPIRequest({
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: "You are an html and tailwinds expert that designs beautiful websites using just html and tailwinds." },
+        { role: "system", content: "You are an html and tailwinds expert that designs beautiful features or full sites using just html and tailwinds, given descriptions of websites or apps and images." },
         { role: "user", content: combinedPromptContent }
       ]
     });
@@ -117,6 +152,7 @@ export default function Home() {
 
     setIsLoading(false);
   }
+
 
   return (
     <div className="flex flex-col h-screen sandy">
@@ -132,7 +168,7 @@ export default function Home() {
       <div className="flex flex-col">
         {!generatedContent && <div className="flex m-10 h-auto sitesy">Your Website Will Appear Here 😆💻</div>}
         {generatedContent &&
-          <div className="flex-grow flex items-center justify-center rounded-lg p-8">
+          <div className="flex items-center justify-center rounded-lg p-4">
             <div className="bg-white shadow-xl rounded-lg w-4/5 transform transition-transform duration-100 hover:scale-105">
               <div dangerouslySetInnerHTML={{ __html: generatedContent }}></div>
             </div>
@@ -143,7 +179,7 @@ export default function Home() {
       </div>
 
       {/* Input, Image Prompt, and Submit */}
-      < div className="w-full p-4 subs fixed bottom-0 left-0 border-t rounded-lg h-auto shadow-xl" >
+      < div className="w-full p-4 subs fixed bottom-0 left-0 border-t rounded-lg h-auto shadow-xl fixed" >
         <input
           type="text"
           placeholder="Enter website idea..."
@@ -151,6 +187,18 @@ export default function Home() {
           value={websiteIdea}
           onChange={(e) => setWebsiteIdea(e.target.value)}
         />
+        <span className="image-upload-control flex flex-grow flex-row w-full h-auto">
+          <input className="flex flex-row w-40 h-auto px-4 py-2 sand text-white rounded-lg mb-2 shadow-xl transform transition-transform duration-100 hover:scale-105 mr-5"
+            ref={fileInputRef}
+            type="file"
+            accept="image/*"
+            onChange={(e) => handleImageUpload(e)}
+          />
+          {uploadedImage && (
+            <button className="flex flex-row w-40 h-auto px-4 py-2 sand text-white rounded-lg mb-2 shadow-xl transform transition-transform duration-100 hover:scale-105 mr-5" onClick={removeUploadedImage}>Remove Image</button>
+          )}
+        </span>
+
         <button
           className="w-full px-4 py-2 sand text-white rounded-lg mb-2 shadow-xl transform transition-transform duration-100 hover:scale-105"
           onClick={handleSubmit}
